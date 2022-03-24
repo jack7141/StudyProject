021401382a24c0c90496c201e1639eef08265773
황광회 <ghl92479@gmail.com>
2022-03-24 15:46:35 +0900

    배포준비

--- Pinterest/config/settings.py
+++ Pinterest/config/settings.py
@@ -80,14 +80,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("RDS_HOST"), #myproject.cgish4s30zrj.us-east-1.rds.amazonaws.com
+            "NAME": os.environ.get("RDS_NAME"),# myproject
+            "USER": os.environ.get("RDS_USER"),#postgresql 
+            "PASSWORD": os.environ.get("RDS_PASSWORD"),#dlqms1004
+            "PORT": "5432",
+        }
+    }    
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
