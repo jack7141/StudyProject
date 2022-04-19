777d460f58bb4b8574034349b7af44e15d64291f
황광회 <ghl92479@gmail.com>
2022-04-19 15:59:53 +0900

    postsql설정 및 DB TDD추가

--- TDD/config/settings/local.py
+++ TDD/config/settings/local.py
@@ -11,9 +11,24 @@ SECRET_KEY = env(
 )
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+# SQLite3
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    }
+}
+"""
+# PostgresSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
