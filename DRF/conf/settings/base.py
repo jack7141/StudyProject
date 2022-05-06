4eca5e133c34da062fd555df25c500136e3a57da
황광회 <ghl92479@gmail.com>
2022-05-07 03:54:46 +0900

    base File 수정

--- DRF/conf/settings/base.py
+++ DRF/conf/settings/base.py
@@ -19,18 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
