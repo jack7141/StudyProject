8854c60190e0f0e9a4ae625af8d47d11e5409b88
황광회 <ghl92479@gmail.com>
2022-05-10 23:50:54 +0900

    swagger url -> path로 수정

--- DRF_Study/api/views.py
+++ DRF_Study/api/views.py
@@ -5,9 +5,14 @@ from .serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """ ModelViewSet 내부에 `create():post`, `retrieve(): get/{id}`, `update(): put`,
-    `partial_update(): patch`, `destroy(): delete`  `list(): get
-
+    """ ModelViewSet method 정리
+    `list(): get',
+    `create():post`,
+    `retrieve(): get/{id}`,
+    `update(): put`,
+    `partial_update(): patch`,
+    `destroy(): delete`
+    - 근데 여기서 내가 원하는 부분만 남기고 지우고 싶으면 어떻게 처리해야하지?
     """
     queryset = User.objects.all().order_by('-date_joined')
     """
@@ -15,13 +20,12 @@ class UserViewSet(viewsets.ModelViewSet):
     출력값을 serialize할 때 사용하는 serializer 클래스. 
     일반적으로 이 속성을 설정하거나 get_serializer_class()메소드로 override해서 사용해야 함
     """
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # serializer_class를 사용하여 설정하거나, get_serializer_class()를 사용하여 처리
+    # serializer_class = UserSerializer
     def get_serializer_class(self):
-        pass
+        return UserSerializer
 
-    def get_queryset(self):
-        pass
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -29,5 +33,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    # serializer_class = GroupSerializer
+    def get_serializer_class(self):
+        return GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
--- DRF_Study/conf/urls.py
+++ DRF_Study/conf/urls.py
@@ -20,12 +20,12 @@ from api import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-
+from django.conf.urls import url
 
 router = routers.DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('groups', views.GroupViewSet)
-
+# router.register('', views.UserViewSet)
 # swagger 연결
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,14 +42,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     # path('api/', include('api.urls'))
 ]
-
-"""
-url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
-url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
-url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-"""
