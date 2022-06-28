d94180269647c44bb474db9c3a353e3cca3612a3
황광회 <ghl92479@gmail.com>
2022-06-28 23:37:57 +0900

    DataStructure 추가

---
+++ Clean-Code/Tips/HandleIF.py
@@ -0,0 +1,8 @@
+
+
+
+def place_a_guess(guesses: int, answer: int, player_name: str):
+    if guessess > 0:
+        print(f'Player {player_name} has {guesses}')
+
+        guess = int
--- FastAPI/Tutorial/app/database/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Company(BaseModel):
-    company_name_ko: Optional[str] = None
-    company_name_en: Optional[str] = None
-    company_name_ja: Optional[str] = None
