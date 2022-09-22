2dc2682620d7139a19b3aa6aa7c4b3c1936a6d1d
황광회 <jack7141@nate.com>
2022-09-22 20:45:07 +0900

    깃정리

--- DRF/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers # serializer import
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = "__all__"
--- DRF_Study/api/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Reviews(models.Model):
-    account_alias = models.CharField(primary_key=True,  max_length=128)
