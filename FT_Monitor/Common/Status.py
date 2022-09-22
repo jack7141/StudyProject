695e43f90195bcc17af099f2e9003ce4740ffae1
황광회 <jack7141@nate.com>
2022-09-22 20:44:37 +0900

    깃 정리

--- FT_Monitor/Common/Status.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-from Common import ConstVar
-
-class Status(Enum):
-    SUCCESS = True
-    FAIL = False
--- Pinterest/reviews/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from . import models
-class review_form(forms.ModelForm):
-    class Meta:
-        model = models.Review
-        fields = ("review",)
-
