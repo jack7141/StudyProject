c5605e5fa8501c79950a07914683abdc05ef7f93
황광회 <jack7141@nate.com>
2022-09-27 17:23:20 +0900

    [strategy_pattern] 전략패턴 구현
    [strategy_pattern] 전략패턴 구현으로 인해 if, else 문을 피하고 사용할 수 있게되었다.

--- strategy_pattern/intro.py
+++ strategy_pattern/intro.py
@@ -3,7 +3,7 @@ import string
 import random
 from typing import List
 from dataclasses import dataclass
-
+from abc import ABC, abstractmethod
 
 logger = logging.getLogger()
 
@@ -18,11 +18,42 @@ def generate_id(length=8):
     """
     return ''.join(random.choices(string.ascii_uppercase, k=length))
 
+
 @dataclass
 class SupportTicket:
     id: str
     customer: str
     issue: str
+
+
+class TicketOrderingStrategy(ABC):
+    @abstractmethod
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        pass
+
+
+class FIFOOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        return list.copy()
+
+
+class FILOOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        list_copy = list.copy()
+        list_copy.reverse()
+        return list_copy
+
+
+class RandomOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        list_copy = list.copy()
+        random.shuffle(list_copy)
+        return list_copy
+
+
+class BlackHoleOrderingStrategy(TicketOrderingStrategy):
+    def create_ordering(self, list: List[SupportTicket]) -> List[SupportTicket]:
+        return []
 
 
 class CustomerSupport:
@@ -32,22 +63,16 @@ class CustomerSupport:
     def create_ticket(self, customer, issue):
         self.tickets.append(SupportTicket(generate_id(), customer, issue))
 
-    def process_tickets(self, processing_strategy: str = "fifo"):
+    def process_tickets(self, processing_strategy: TicketOrderingStrategy):
+        ticket_list = processing_strategy.create_ordering(self.tickets)
+
         if len(self.tickets) == 0:
             logger.info("There are no tickets to process. Well done!")
             return
 
-        if processing_strategy == 'fifo':
-            for ticket in self.tickets:
-                self.process_ticket(ticket)
-        elif processing_strategy == 'filo':
-            for ticket in reversed(self.tickets):
-                self.process_ticket(ticket)
-        elif processing_strategy == 'random':
-            list_copy = self.tickets.copy()
-            random.shuffle(list_copy)
-            for ticket in list_copy:
-                self.process_ticket(ticket)
+        for ticket in ticket_list:
+            self.process_ticket(ticket)
+
 
     def process_ticket(self, ticket: SupportTicket):
         logger.info("===========================================")
@@ -62,8 +87,8 @@ if __name__ == '__main__':
 
     app = CustomerSupport()
 
-    app.create_ticket("John Smith", "My computer makes strange sounds!")
-    app.create_ticket("Linux Smith", "sounds!")
-    app.create_ticket("Arjan Eggs Smith", "Pycharms")
+    app.create_ticket("1", "My computer makes strange sounds!")
+    app.create_ticket("2", "sounds!")
+    app.create_ticket("3", "Pycharms")
 
-    app.process_tickets("filo")
+    app.process_tickets(FILOOrderingStrategy())
