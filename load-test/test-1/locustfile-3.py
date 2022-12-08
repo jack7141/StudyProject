7888c6e921daeb0f0963639199167b1aeadfe9ae
황광회 <jack7141@nate.com>
2022-12-08 11:40:30 +0900

    locust 추가

---
+++ load-test/test-1/locustfile-3.py
@@ -0,0 +1,109 @@
+import sys
+import threading
+import time
+
+from collections import defaultdict
+from locust import HttpUser, task, between
+
+global is_done
+is_done = False
+
+class Order:
+    def __init__(self, account_alias):
+        self.account_alias = account_alias
+
+    def make_order(self, client):
+        print(f"REQUEST create order account_alias: {self.account_alias}")
+
+        response = client.post(
+            url="/api/v1/hanaw/accounts/global/fractional/order",
+            json={
+                "account_alias": self.account_alias[:-3],
+                "account_type": self.account_alias[-3:],
+                "code": "MSFT",
+                "amount": "1000"
+            }
+        )        
+
+        if response.ok:
+            order_no = response.json().get('order_no')            
+            print(f"SUCCEEDED create order_no: {order_no}")
+            return order_no
+        else:
+            print(f"FAILED create order, account_alias : {self.account_alias}, error: {response.text}")
+            return None
+                
+
+
+class HanawOrderTest(HttpUser):
+    #wait_time = between(0.2, 0.5)
+
+    lock = threading.Lock()
+    results = defaultdict(list)
+    accounts = [
+        'A25033952010',
+        'A31309604010',
+        'A98081538010',
+        'A35740157010',
+        'A57639682010',
+        'A09834287010',
+        'A03596514010',
+        'A56543189010',
+        'A20926582010',
+        'A86640858010',
+    ]
+
+    @task(weight=1)
+    def account_8(self):
+        account_alias = self.accounts[8]
+
+        order = Order(account_alias=account_alias)
+        order_no = order.make_order(client=self.client)
+
+        if order_no:
+            self.results[account_alias].append(order_no)  
+
+    @task(weight=1)
+    def account_9(self):
+        account_alias = self.accounts[9]
+
+        order = Order(account_alias=account_alias)
+        order_no = order.make_order(client=self.client)
+
+        if order_no:
+            self.results[account_alias].append(order_no)
+
+
+    # 테스트 종료시 요청했던 주문을 모두 취소한다.
+    def on_stop(self):
+        global is_done
+
+        self.lock.acquire()
+        if is_done:
+            return 
+
+        for account_alias in self.results.keys():
+
+            print(f'start to cancel order, account_alias : {account_alias}')
+
+            for order_no in self.results.get(account_alias, []):
+                print(f"REQUEST cancel order_no : {order_no}")
+
+                response = self.client.post(
+                    url="/api/v1/hanaw/accounts/global/fractional/order/cancel",
+                    json={
+                        "account_alias": account_alias[:-3],
+                        "account_type": account_alias[-3:],
+                        "order_no": order_no
+                    }
+                )
+
+                if response.ok:
+                    print(f"SUCCEEDED cancel order no : {order_no}")
+                else:
+                    print(f"FAILED cancel order_no : {order_no}, error: {response.text}")
+                    return             
+
+            is_done = True
+                            
+        self.lock.release()
